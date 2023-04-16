19ae7417ff0495de1d36de9b2265f74b41e5159a
Abdelrahman A.Z <abu.24hr4.1995@outlook.com>
2023-04-16 03:16:57 +0300

    initial commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @foreach ($posts as $post)
         <article>
 
-            <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            <h1><a href="/posts/{{ $post->id }}">{!! $post->title !!}</a></h1>
 
             <div>
                 <h2>{{ $post->excerpt }}</h2>
